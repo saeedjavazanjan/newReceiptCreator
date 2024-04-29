38472377b736afdfce4a742ba75e9f4319ffe0b4
saeed <ghfarisaeed1@gmail.com>
2024-04-29 19:38:30 +0330

    register

---
+++ app/src/main/java/com/saeed/zanjan/receipt/presentation/components/CustomDropDown.kt
@@ -0,0 +1,96 @@
+package com.saeed.zanjan.receipt.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuItemColors
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomDropdown(
+    items: List<String>,
+    selectedItem: String,
+    onItemSelected: (String) -> Unit,
+    label: String,
+    modifier: Modifier = Modifier,
+    isError:Boolean
+) {
+    var expanded by remember { mutableStateOf(false) }
+    val focusRequester = FocusRequester()
+
+    Column(
+        modifier = modifier
+    ) {
+        OutlinedTextField(
+            value = selectedItem,
+            isError=isError,
+            onValueChange = { },
+            label = { Text(label) },
+            trailingIcon = {
+                IconButton(
+                    onClick = { expanded = !expanded }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Toggle Dropdown"
+                    )
+                }
+            },
+            readOnly = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { expanded = !expanded }
+                .focusRequester(focusRequester)
+                .onFocusChanged {
+                    if (it.isFocused) {
+                        expanded = !expanded
+                    }
+                },
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp, end = 10.dp)
+                .background(Color.White)
+        ) {
+            items.forEach { item ->
+                DropdownMenuItem(text = {
+                    Text(text = item)
+                },
+                    onClick = {
+                        onItemSelected(item)
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/saeed/zanjan/receipt/presentation/ui/registration/RegistrationScreen.kt
+++ app/src/main/java/com/saeed/zanjan/receipt/presentation/ui/registration/RegistrationScreen.kt
@@ -1,5 +1,7 @@
 package com.saeed.zanjan.receipt.presentation.ui.registration
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -9,30 +11,36 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.saeed.zanjan.receipt.domain.RegistrationInfo
-import com.saeed.zanjan.receipt.presentation.navigation.Screen
-import kotlinx.coroutines.coroutineScope
+import com.saeed.zanjan.receipt.presentation.components.CustomDropdown
 import kotlinx.coroutines.launch // Import launch function
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RegistrationScreen(
@@ -45,252 +53,191 @@ fun RegistrationScreen(
     var phone by remember { mutableStateOf("") }
     var userId by remember { mutableStateOf("") }
     var jobType by remember { mutableStateOf("") }
+
+    var expandedjobtype by remember { mutableStateOf(false) }
+
     var isSignInTabSelected by remember { mutableStateOf(false) }
+
+    var sendOtpClicked by remember { mutableStateOf(false) }
+
+    var isOtpScreenVisible by remember { mutableStateOf(false) }
+
+    var error by remember { mutableStateOf(false) }
+    val jobTypes = listOf("خیاطی", "خشکشویی", "تعمیرات", "سایر")
 
     val snackbarHostState = SnackbarHostState()
     val coroutineScope = rememberCoroutineScope()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        // Tabs for sign-in and sign-up
-        TabRow(
-            selectedTabIndex = if (isSignInTabSelected) 0 else 1,
-            modifier = Modifier.fillMaxWidth().height(56.dp).padding(10.dp)        ) {
-            Tab(
-                selected = isSignInTabSelected,
-                onClick = { isSignInTabSelected = true }
-            ) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxHeight() // Ensure the tab fills the entire height
-                        .padding(vertical = 8.dp, horizontal = 16.dp),
-                    text = "Sign In",
-                    style = MaterialTheme.typography.bodyMedium
-                )
+    if (isOtpScreenVisible) {
+        OtpScreen(
+            onSendOtpClicked = {
+                // Implement sending OTP logic here
+            },
+            onDismiss = {
+                isOtpScreenVisible = false
             }
-            Tab(
-                selected = !isSignInTabSelected,
-                onClick = { isSignInTabSelected = false }
-            ) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxHeight() // Ensure the tab fills the entire height
-                        .padding(vertical = 8.dp, horizontal = 16.dp),
-                    text = "ورود",
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-        }
-
-        // Content based on selected tab
-        if (isSignInTabSelected) {
-            // Sign In tab
-            OutlinedTextField(
-                value = phone,
-                onValueChange = { phoneNumber ->
-                    phone=phoneNumber
-                },
-                label = { Text("شماره تلفن") },
-                isError = phone.isEmpty() || phone.length < 11,
-                singleLine = true,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
-                modifier = Modifier.fillMaxWidth()
-            )
-            Button(
-                onClick = {
-                    // Sign In button clicked
-                    onSignInClicked(Screen.Otp.route)
-
-                    // Switch to OTP screen
-                   // onSendOtpClicked()
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("ورود")
-            }
-        } else {
-            // Sign Up tab
-            OutlinedTextField(
-                value = companyName,
-                onValueChange = { companyName = it },
-                label = { Text("نام مجموعه") },
-                modifier = Modifier.fillMaxWidth()
-            )
-            OutlinedTextField(
-                value = address,
-                onValueChange = { address = it },
-                label = { Text("آدرس") },
-                modifier = Modifier.fillMaxWidth()
-            )
-            OutlinedTextField(
-                value = phone,
-                onValueChange = { phoneNumber ->
-                  phone=phoneNumber
-                },
-                label = { Text("شماره تلفن") },
-                isError = phone.isEmpty() || phone.length < 11,
-                singleLine = true,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
-                modifier = Modifier.fillMaxWidth()
-            )
-            OutlinedTextField(
-                value = userId,
-                onValueChange = { userId = it },
-                label = { Text("آیدی پیج کاری") },
-                modifier = Modifier.fillMaxWidth()
-            )
-            OutlinedTextField(
-                value = jobType,
-                onValueChange = { jobType = it },
-                label = { Text("عنوان شغل") },
-                modifier = Modifier.fillMaxWidth()
-            )
-            Button(
-                onClick = {
-                    if (phone.isEmpty() || phone.length < 11) {
-                        coroutineScope.launch {
-                            snackbarHostState.showSnackbar("شماره تلفن را به درستی وارد کنید")
-                        }
-
-                    }else{
-                        val registrationInfo = RegistrationInfo(
-                            companyName = companyName,
-                            address = address,
-                            phone = phone,
-                            userId = userId,
-                            jobType = jobType
-                        )
-                        // Handle sign-up logic
-                        onSignUpClicked(registrationInfo)
-                        // Switch to OTP screen
-                     //   onSendOtpClicked()
-                    }
-                    // Sign Up button clicked
-
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("ثبت نام")
-            }
-        }
+        )
     }
-}
-/*
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun RegistrationScreen(
-    onSignInClicked: (String) -> Unit,
-    onSignUpClicked: (RegistrationInfo) -> Unit,
-    onSendOtpClicked: () -> Unit
-) {
-    var companyName by remember { mutableStateOf("") }
-    var address by remember { mutableStateOf("") }
-    var phone by remember { mutableStateOf("") }
-    var userId by remember { mutableStateOf("") }
-    var jobType by remember { mutableStateOf("") }
-    var isSignInTabSelected by remember { mutableStateOf(false) }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        // Tabs for sign-in and sign-up
-        TabRow(
 
-            selectedTabIndex = if (isSignInTabSelected) 0 else 1,
-            modifier = Modifier.fillMaxWidth().height(56.dp).padding(10.dp),
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) }
+
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            Tab(
-                selected = isSignInTabSelected,
-                onClick = { isSignInTabSelected = true }
-            ) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxHeight() // Ensure the tab fills the entire height
-                        .padding(vertical = 8.dp, horizontal = 16.dp),
-                    text = "Sign In")
-            }
-            Tab(
 
-                selected = !isSignInTabSelected,
-                onClick = { isSignInTabSelected = false }
-            ) {
-                Text( modifier = Modifier
-                    .fillMaxHeight() // Ensure the tab fills the entire height
-                    .padding(vertical = 8.dp, horizontal = 16.dp),
-                    text = "Sign Up")
-            }
-        }
-
-        // Content based on selected tab
-        if (isSignInTabSelected) {
-            // Sign In tab
-            TextField(
-                value = phone,
-                onValueChange = { phone = it },
-                label = { Text("Phone Number") }
-            )
-            Button(
-                onClick = {
-                    // Sign In button clicked
-                    onSignInClicked(phone)
-                    // Switch to OTP screen
-                    onSendOtpClicked()
-                }
-            ) {
-                Text("Sign In")
-            }
-        } else {
-            // Sign Up tab
-            TextField(
-                value = companyName,
-                onValueChange = { companyName = it },
-                label = { Text("Company Name") }
-            )
-            TextField(
-                value = address,
-                onValueChange = { address = it },
-                label = { Text("Address") }
-            )
-            TextField(
-                value = phone,
-                onValueChange = { phone = it },
-                label = { Text("Phone Number") }
-            )
-            TextField(
-                value = userId,
-                onValueChange = { userId = it },
-                label = { Text("User ID") }
-            )
-            TextField(
-                value = jobType,
-                onValueChange = { jobType = it },
-                label = { Text("Job Type") }
-            )
-            Button(
-                onClick = {
-                    // Sign Up button clicked
-                    val registrationInfo = RegistrationInfo(
-                        companyName = companyName,
-                        address = address,
-                        phone = phone,
-                        userId = userId,
-                        jobType = jobType
+            // Tabs for sign-in and sign-up
+            TabRow(
+                selectedTabIndex = if (isSignInTabSelected) 0 else 1,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+                    .padding(10.dp)        ) {
+                Tab(
+                    selected = isSignInTabSelected,
+                    onClick = { isSignInTabSelected = true }
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxHeight() // Ensure the tab fills the entire height
+                            .padding(vertical = 8.dp, horizontal = 16.dp),
+                        text = "ورود",
+                        style = MaterialTheme.typography.bodyMedium
                     )
-                    // Handle sign-up logic
-                    onSignUpClicked(registrationInfo)
-                    // Switch to OTP screen
-                    onSendOtpClicked()
                 }
-            ) {
-                Text("Sign Up")
+                Tab(
+                    selected = !isSignInTabSelected,
+                    onClick = { isSignInTabSelected = false }
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxHeight() // Ensure the tab fills the entire height
+                            .padding(vertical = 8.dp, horizontal = 16.dp),
+                        text = "ثبت نام",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
+
+            // Content based on selected tab
+            if (isSignInTabSelected) {
+                // Sign In tab
+                OutlinedTextField(
+                    value = phone,
+                    onValueChange = { phoneNumber ->
+                        phone=phoneNumber
+                    },
+                    label = { Text("شماره تلفن") },
+                    isError = (phone.isEmpty() || phone.length < 11)&&sendOtpClicked,
+
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Button(
+                    onClick = {
+                        // Sign In button clicked
+                        sendOtpClicked=true
+                        if (phone.isEmpty() || phone.length < 11) {
+                            coroutineScope.launch {
+                                snackbarHostState.showSnackbar("شماره تلفن را به درستی وارد کنید")
+                            }
+
+                        }else {
+                          //  onSignInClicked(Screen.Otp.route)
+                            isOtpScreenVisible = true
+
+                        }
+                        // Switch to OTP screen
+                        // onSendOtpClicked()
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("ثبت")
+                }
+            } else {
+                // Sign Up tab
+                OutlinedTextField(
+                    value = companyName,
+                    onValueChange = { companyName = it },
+                    label = { Text("نام مجموعه") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                OutlinedTextField(
+                    value = address,
+                    onValueChange = { address = it },
+                    label = { Text("آدرس") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                OutlinedTextField(
+                    value = phone,
+                    onValueChange = { phoneNumber ->
+                        phone=phoneNumber
+                    },
+                    label = { Text("شماره تلفن") },
+                    isError = (phone.isEmpty() || phone.length < 11)&&sendOtpClicked,
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+                    modifier = Modifier.fillMaxWidth()
+                )
+                OutlinedTextField(
+                    value = userId,
+                    onValueChange = { userId = it },
+                    label = { Text("آیدی پیج کاری") },
+                    modifier = Modifier.fillMaxWidth(),
+                    isError = error,
+
+                )
+                CustomDropdown(
+                    items = listOf("خیاطی", "خشکشویی", "تعمیرات", "سایر"),
+                    selectedItem = jobType,
+                    onItemSelected = { selected ->
+                        jobType = selected
+                    },
+                    label = "عنوان شغلی را انتخاب کنید",
+                    modifier = Modifier.fillMaxWidth(),
+                    isError = jobType == "" && sendOtpClicked
+                )
+                Button(
+                    onClick = {
+                        sendOtpClicked=true
+
+                        if (phone.isEmpty() || phone.length < 11) {
+                            coroutineScope.launch {
+                                snackbarHostState.showSnackbar("شماره تلفن را به درستی وارد کنید")
+                            }
+
+                        }else{
+                            val registrationInfo = RegistrationInfo(
+                                companyName = companyName,
+                                address = address,
+                                phone = phone,
+                                userId = userId,
+                                jobType = jobType
+                            )
+                            // Handle sign-up logic
+                          //  onSignUpClicked(registrationInfo)
+                            isOtpScreenVisible = true
+
+                            // Switch to OTP screen
+                            //   onSendOtpClicked()
+                        }
+                        // Sign Up button clicked
+
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("ثبت")
+                }
             }
         }
     }
+
 }
-*/
+
