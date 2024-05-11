eb8287002d456af68601ea826318bef912c07c7c
saeed <ghfarisaeed1@gmail.com>
2024-05-11 19:21:09 +0330

    home design

--- app/src/main/java/com/saeed/zanjan/receipt/presentation/ui/home/Home.kt
+++ app/src/main/java/com/saeed/zanjan/receipt/presentation/ui/home/Home.kt
@@ -8,36 +8,34 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
@@ -45,71 +43,266 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.saeed.zanjan.receipt.R
 import com.saeed.zanjan.receipt.ui.theme.CustomColors
+import com.saeed.zanjan.receipt.ui.theme.NewReceiptCreatorTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Home(){
+fun Home() {
 
     var isSearchExpanded by remember { mutableStateOf(false) }
-
+    val focusRequester = remember { FocusRequester() }
+    LaunchedEffect(isSearchExpanded) {
+        if (isSearchExpanded) {
+            focusRequester.requestFocus()
+        }
+    }
+    NewReceiptCreatorTheme(
+        displayProgressBar=false
+    ) {
     Scaffold(
-       /* topBar = {
-            TopAppBar(
-                title = { Text("My App") },
-                navigationIcon = {
+        /* topBar = {
+             TopAppBar(
+                 title = { Text("My App") },
+                 navigationIcon = {
 
-                },
-                actions = {
-                    IconButton(
-                        onClick = {
-                        //    Handle filter action
-                        }
-                    ) {
-                        Icon(Icons.Default.Star, contentDescription = null)
-                    }
-                }
-            )
-        }*/
+                 },
+                 actions = {
+                     IconButton(
+                         onClick = {
+                         //    Handle filter action
+                         }
+                     ) {
+                         Icon(Icons.Default.Star, contentDescription = null)
+                     }
+                 }
+             )
+         }*/
     ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = 16.dp)
+                .padding(horizontal = 10.dp)
         ) {
-            AnimatedVisibility(
-                visible = !isSearchExpanded,
-                enter = fadeIn(),
-                exit = fadeOut()
-            ) {
 
-                Row {
+
+            Row(modifier = Modifier.padding(vertical = 15.dp)) {
+                Button(
+                    contentPadding = PaddingValues(0.dp),
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(50.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = CustomColors.lightGray
+                    ),
+                    onClick = {
+                        //    Handle navigation
+                    },
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.menu),
+                        tint = CustomColors.darkPurple,
+                        contentDescription = null
+                    )
+                }
+                Spacer(modifier = Modifier.width(10.dp))
+
+                Button(
+                    contentPadding = PaddingValues(0.dp),
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(50.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = CustomColors.lightGray
+                    ),
+                    onClick = {
+                        //    Handle navigation
+                    },
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.filter),
+                        tint = CustomColors.darkPurple,
+                        contentDescription = null
+                    )
+                }
+                Spacer(modifier = Modifier.width(10.dp))
+                Card(
+                    shape = RoundedCornerShape(30.dp),
+                    colors = CardDefaults.cardColors(
+                        contentColor = CustomColors.lightGray,
+                        containerColor = CustomColors.lightGray
+                    )
+                ) {
+                    Row {
+                        AnimatedVisibility(
+                            visible = !isSearchExpanded,
+                            enter = fadeIn(),
+                            exit = fadeOut()
+                        ) {
+                            Button(
+                                contentPadding = PaddingValues(0.dp),
+                                modifier = Modifier
+                                    .clip(CircleShape)
+                                    .size(50.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = CustomColors.lightGray
+                                ),
+                                onClick = {
+                                    isSearchExpanded = true
+                                },
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.search___),
+                                    tint = CustomColors.darkPurple,
+                                    contentDescription = null
+                                )
+                            }
+                        }
+                        AnimatedVisibility(
+                            visible = isSearchExpanded,
+                            enter = slideInHorizontally(initialOffsetX = { fullWidth -> fullWidth }, animationSpec = spring()),
+                            exit = slideOutHorizontally(targetOffsetX = { fullWidth -> fullWidth }, animationSpec = spring())
+                        ) {
+                            TextField(
+                                enabled = true,
+                                value = "",
+                                onValueChange = {
+
+                                },
+                                trailingIcon = {
+                                    Icon(
+                                        modifier = Modifier.clickable {
+                                            isSearchExpanded = false
+                                        },
+                                        painter = painterResource(id = R.drawable.close),
+                                        tint = CustomColors.darkPurple,
+                                        contentDescription = null
+                                    )
+                                },
+                                //  label = { Text("جستجو") },
+                                // isError = (phone.isEmpty() || phone.length < 11) && sendOtpClicked,
+
+                                singleLine = true,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .focusRequester(focusRequester),
+                                shape = RoundedCornerShape(30.dp),// Adjust the radius for rounded corners
+                                colors = TextFieldDefaults.outlinedTextFieldColors(
+                                    containerColor = Color.Transparent, // Set light gray background
+                                    cursorColor = CustomColors.darkPurple,
+                                    focusedBorderColor = CustomColors.lightGray, // Transparent to clear the outline
+                                    unfocusedBorderColor = Color.Transparent // Transparent to clear the outline
+                                ),
+                            )
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                AnimatedVisibility(
+                    visible = !isSearchExpanded,
+                    enter = fadeIn(),
+                    exit = fadeOut()
+                ) {
+                Card(
+
+                    shape= CircleShape,
+                    colors=CardDefaults.cardColors(
+                        containerColor = CustomColors.lightBlue,
+                        contentColor = CustomColors.lightBlue
+                    ),
+
+
+                ) {
+                    // Card content: Name and circular image
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(vertical = 5.dp, horizontal = 10.dp)
+                    ) {
+                        Text("سعید غفاری",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = Color.White
+                            )
+                        Spacer(modifier = Modifier.width(10.dp))
+
+                        Image(
+                            painter = painterResource(id = R.drawable.profile),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(40.dp)
+                                .clip(CircleShape)
+                        )
+
+                    }
+                }
+}
+            }
+             LazyColumn(
+                          modifier = Modifier.weight(1f)
+                      ) {
+                          // Main lazy column content
+                      }
+
+            Card(
+                elevation=CardDefaults.cardElevation(20.dp),
+                shape= CircleShape,
+                colors=CardDefaults.cardColors(
+                    containerColor = CustomColors.lightBlue,
+                    contentColor = CustomColors.lightBlue
+                ),
+
+
+                ) {
+                // Card content: Name and circular image
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .padding( 5.dp)
+                        .fillMaxWidth()
+                ) {
+
+                    Text(
+                        modifier = Modifier.padding(10.dp),
+                       text =  "افزودن رسید جدید",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color.White
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+
                     Button(
+                        contentPadding = PaddingValues(0.dp),
                         modifier = Modifier
-                            .wrapContentWidth()
-                            .wrapContentHeight(),
+                            .clip(CircleShape)
+                            .size(50.dp),
                         colors = ButtonDefaults.buttonColors(
-                            containerColor = CustomColors.CustomLightGray
+                            containerColor = CustomColors.darkBlue
                         ),
                         onClick = {
-                            //    Handle navigation
-                        },
-                        shape = CircleShape
-                    ) {
-                        Icon(    painter = painterResource(id = R.drawable.menu),
 
-                            contentDescription = null)
+                        },
+                    ) {
+                        Icon(
+                             Icons.Default.Add,
+                            tint = Color.White,
+                            contentDescription = null
+                        )
                     }
-                    Card(
+
+
+                }
+            }
+            Spacer(modifier = Modifier.size(20.dp))
+
+            /* Card(
                         modifier = Modifier
                             .padding(vertical = 16.dp)
-                            .fillMaxWidth()
+
                     ) {
                         // Card content: Name and circular image
                         Row(
@@ -126,12 +319,12 @@ fun Home(){
                             Spacer(modifier = Modifier.width(16.dp))
                             Text("User Name", style = TextStyle(fontSize = 20.sp))
                         }
-                    }
-                }
+                    }*/
 
-            }
 
-            if (isSearchExpanded) {
+        }
+
+        /*      if (isSearchExpanded) {
                 AnimatedVisibility(
                     visible = isSearchExpanded,
                     enter = slideInHorizontally(initialOffsetX = { fullWidth -> fullWidth }, animationSpec = spring()),
@@ -148,7 +341,7 @@ fun Home(){
                             }
                         },
                         trailingIcon = {
-                            IconButton(onClick = { /* Clear search */ }) {
+                            IconButton(onClick = { *//* Clear search *//* }) {
                                 Icon(Icons.Default.Clear, contentDescription = null)
                             }
                         }
@@ -167,15 +360,11 @@ fun Home(){
                     Spacer(modifier = Modifier.width(16.dp))
                     // Your other content in the row
                 }
-            }
+            }*/
 
-            LazyColumn(
-                modifier = Modifier.weight(1f)
-            ) {
-                // Main lazy column content
-            }
 
-            Card(
+
+        /*     Card(
                 modifier = Modifier
                     .padding(vertical = 16.dp)
                     .fillMaxWidth()
@@ -187,11 +376,12 @@ fun Home(){
                 ) {
                     Text("Bottom Card Content")
                     Spacer(modifier = Modifier.weight(1f))
-                    IconButton(onClick = { /* Handle plus button click */ }) {
+                    IconButton(onClick = { *//* Handle plus button click *//* }) {
                         Icon(Icons.Default.Add, contentDescription = null)
                     }
                 }
-            }
-        }
+            }*/
     }
+}
+
 }
--- app/src/main/java/com/saeed/zanjan/receipt/ui/theme/CustomColors.kt
+++ app/src/main/java/com/saeed/zanjan/receipt/ui/theme/CustomColors.kt
@@ -3,6 +3,8 @@ package com.saeed.zanjan.receipt.ui.theme
 import androidx.compose.ui.graphics.Color
 
 object CustomColors {
-    val CustomLightGray = Color(0xFFECEAEA)
-
+    val lightGray = Color(0xFFECEAEA)
+    val darkPurple= Color(0xFF515369)
+    val lightBlue= Color(0xff328ce2)
+    val darkBlue= Color(0xff0056ab)
 }
